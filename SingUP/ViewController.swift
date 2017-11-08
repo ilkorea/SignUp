4e5aa22d6132b94bc03447ae662565f6bead6b4d
박성진_MacBook <ilk486@naver.com>
2017-11-08 20:22:53 +0900

    키보드사용법 프로토콜 델리게이트 설명됨

--- SingUP/ViewController.swift
+++ SingUP/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var telTextField: UITextField!
+    @IBOutlet weak var blogTextField: UITextField!
+    @IBOutlet weak var resultTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //nameTextField.delegate = self
     }
 
+    @IBAction func signUpAction(_ sender: AnyObject) {
+        resultTextView.text = "\(nameTextField.text)  님 가입을 축하 합니다."
+    }
+  
+    //textField 에서 엔터가 입력될 경우 호출
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //nameTextField.resignFirstResponder()//엔터입력 이벤트
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //바탕화면 터치시 반응
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)//키보드가 비활성화
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
